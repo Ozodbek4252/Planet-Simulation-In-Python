31c49d2a8d45e0f181b35ec0b974a30a56307498
Ozodbek4252 <ozodbekmcfly@gmail.com>
2022-03-22 22:11:01 +0500

    Initializing Planets

--- tutorial.py
+++ tutorial.py
@@ -7,6 +7,10 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GRAY = (80, 78, 81)
 
 class Planet:
   AU = 149.6e6 * 1000 # AU = astronomical units
@@ -37,14 +41,32 @@ def main():
   run = True
   clock = pygame.time.Clock()
 
+  sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+  sun.sun = True
+
+  mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GRAY, 3.3 * 10**23)
+
+  venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+
+  earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+
+  mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+
+  planets = [sun, mercury, venus, earth, mars]
+
   while run:
     clock.tick(60)
     # WIN.fill(WHITE)
-    # pygame.display.update()
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
+
+    for planet in planets:
+      planet.draw(WIN)
+    
+    pygame.display.update()
+
 
   pygame.quit()
 
