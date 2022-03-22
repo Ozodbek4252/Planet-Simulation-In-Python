115ffad201a9752cbd9bd1a9cd5c70ddf9e2bda4
Ozodbek4252 <ozodbekmcfly@gmail.com>
2022-03-22 21:54:41 +0500

    Creating planets

--- tutorial.py
+++ tutorial.py
@@ -8,14 +8,39 @@ pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
 
+class Planet:
+  AU = 149.6e6 * 1000 # AU = astronomical units
+  G = 6.67428e-11
+  SCALE = 250 / AU  # 1AU = 100 pixels
+  TIMESTEP = 3600*24  # 1 day
+
+  def __init__(self, x, y, radius, color, mass):
+    self.x = x
+    self.y = y
+    self.radius = radius
+    self.color = color
+    self.mass = mass
+
+    self.orbit = []
+    self.sun = False
+    self.distance_to_sun = 0
+
+    self.x_vel = 0
+    self.y_vel = 0
+
+  def draw(self, win):
+    x = self.x * self.SCALE + WIDTH / 2
+    y = self.y * self.SCALE + HEIGHT / 2
+    pygame.draw.circle(win, self.color, (x, y), self.radius)
+
 def main():
   run = True
   clock = pygame.time.Clock()
 
   while run:
     clock.tick(60)
-    WIN.fill(WHITE)
-    pygame.display.update()
+    # WIN.fill(WHITE)
+    # pygame.display.update()
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
